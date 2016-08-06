777db2b373dafca9a7a4d97383e949a0a1c92a25
Bennett Batzli <bennett.batzli@gmail.com>
2016-08-06 11:43:21 -0500

    Base number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,29 +2,35 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(101)
-# message = "Guess a number between 1 and 100."
-guess_int = nil
+message = "Guess a number between 1 and 100."
+# guess_int = nil
 
 get '/' do
+  # erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  if params['guess'] != nil
+    guess_int = params['guess'].to_i
 
+    message = check_guess(guess_int)
 
-  message = check_guess(guess_int)
-
+  end
   erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 
   # erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :guess => guess_int}
 
 end
 
-def check_guess(guess_int)
-  guess_int = params['guess'].to_i
-  if guess_int > SECRET_NUMBER
-    return "Your guess is too high!"
-  elsif guess_int < SECRET_NUMBER
-    return  "Your guess is too low!"
-  elsif guess_int == SECRET_NUMBER
-    return  "That's right!"
-  elsif guess_int.nil?
-    return  "Guess a number between 1 and 100."
+def check_guess(guess)
+  if guess.nil?
+    "Guess a number between 1 and 100."
+  elsif guess > SECRET_NUMBER + 5
+    "WAYYY too high!"
+  elsif guess > SECRET_NUMBER
+    "Your guess is too high!"
+  elsif guess < SECRET_NUMBER - 5
+    "WAYYY too low!"
+  elsif guess < SECRET_NUMBER
+    "Your guess is too low!"
+  elsif guess == SECRET_NUMBER
+    "That's right! The secret number is #{guess}!"
   end
 end
