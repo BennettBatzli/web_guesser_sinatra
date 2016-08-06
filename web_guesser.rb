295517807c5ea1efa0d8c3a4c91a220c6000fd51
Bennett Batzli <bennett.batzli@gmail.com>
2016-08-05 23:15:51 -0500

    added erb template and html. added logic to check guess. currently defaults to the 'too low' message

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,29 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(101)
+# message = "Guess a number between 1 and 100."
+guess_int = nil
 
 get '/' do
-  "the secret number is: #{SECRET_NUMBER}"
+
+
+  message = check_guess(guess_int)
+
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+
+  # erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :guess => guess_int}
+
+end
+
+def check_guess(guess_int)
+  guess_int = params['guess'].to_i
+  if guess_int > SECRET_NUMBER
+    return "Your guess is too high!"
+  elsif guess_int < SECRET_NUMBER
+    return  "Your guess is too low!"
+  elsif guess_int == SECRET_NUMBER
+    return  "That's right!"
+  elsif guess_int.nil?
+    return  "Guess a number between 1 and 100."
+  end
 end
